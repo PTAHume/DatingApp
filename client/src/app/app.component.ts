eec42a6bd005a8591b79cfa1a16961fec014b239
Patrick Hume <patricktahume@outlook.com>
2024-06-05 11:33:06 +0100

    fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
-
-export class AppComponent implements OnInit{
-
-  title:string = 'Dating App';
-  users: any;
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.http.get('https://localhost:5001/api/users').subscribe( {
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    });
-  }
+export class AppComponent {
+  title = 'client';
 }
